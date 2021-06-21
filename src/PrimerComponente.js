652f2779f6b2cb513c0cd2ece796f5ec8b14ca28
iedelstein <iara.edelstein@mercadolibre.com>
2021-06-21 20:17:52 -0300

    changes welcome

---
+++ src/PrimerComponente.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function PrimerComponente() {
+  const curso = 'React inicial - 101';
+  const variableAMostrar = <h1>Bienvenido al curso {curso}</h1>;
+  return variableAMostrar;
+}
